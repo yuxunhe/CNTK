fdb2a18770dac62fa2e0b465b0c417a659061f10
liqun fu <liqfu@microsoft.com>
2017-09-27 13:00:21 -0700

    undo Main.cpp in V2LibE2ETest

--- Tests/EndToEndTests/CNTKv2Library/EndToEndTests/Main.cpp
+++ Tests/EndToEndTests/CNTKv2Library/EndToEndTests/Main.cpp
@@ -11,8 +11,6 @@
 
 #include <iostream>
 #include <cstdio>
-#include <locale>
-#include <codecvt>
 
 using namespace CNTK;
 using namespace std::placeholders;
@@ -25,80 +23,6 @@ void TrainTruncatedLSTMAcousticModelClassifier();
 void TestFrameMode();
 void TestDistributedCheckpointing();
 
-// #include "../proto/onnx/onnx.h";
-// #include "../proto/onnx/CNTKToONNX.h"
-// #include "../proto/onnx/ONNXToCNTK.h"
-// #include "../proto/onnx/core/graph.h"
-
-std::string ToString(const std::wstring& wstring)
-{
-    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
-    return converter.to_bytes(wstring);
-}
-
-static void PrintGraph(const FunctionPtr& function, int spaces, bool useName = false)
-{
-    std::vector<Variable> inputs = function->Inputs();
-    size_t count = inputs.size();
-    if (count == 0)
-    {
-        std::cout << std::string(spaces, '.') + "(" + ToString(useName ? function->Name() : function->Uid()) + ")" + ToString(function->AsString()) << std::endl;
-        return;
-    }
-
-    for (Variable input : inputs)
-    {
-        std::cout << std::string(spaces, '.') + "(" + ToString(useName ? function->Name() : function->Uid()) + ")" + "->" +
-            "(" + ToString(useName ? input.Name() : input.Uid()) + ")" + ToString(input.AsString()) << std::endl;
-    }
-
-    for (Variable input : inputs)
-    {
-        if (input.Owner() != NULL)
-        {
-            FunctionPtr f = input.Owner();
-            PrintGraph(f, spaces + 4);
-        }
-    }
-}
-
-void RunLotus(DeviceDescriptor device)
-{
-    {
-        // vgg model load failed
-        //const std::wstring vgg16 = L"E:/LiqunWA/CNTK/ONNX/vgg16/vgg16/graph.pb";
-        //FunctionPtr cntkModel = Function::Load(vgg16, device, ModelFormat::ONNX);
-    }
-
-    // CNTK function saved as ONNX. 
-    // Loaded but graph is not the same as in memory graph. See ONNX.Save commented code.
-    // FunctionPtr cntkModel = Function::Load(savedONNXModelFile, device, ModelFormat::ONNX);
-
-    // following are experiment code
-    {
-        const std::wstring savedONNXModelFile = L"E:/LiqunWA/CNTK/ONNX/MNISTConvolutionONNX.model";
-        const std::wstring cntkModelFile = L"E:/LiqunWA/CNTK/ONNX/MNISTConvolution.model";
-        const std::wstring cntkModelFile2 = L"E:/LiqunWA/CNTK/ONNX/MNISTConvolution.model";
-
-        //const std::wstring cntkModelFile = L"E:/LiqunWA/CNTK/ONNX/MNISTMLP.model";
-        //const std::wstring savedONNXModelFile = L"E:/LiqunWA/CNTK/ONNX/MNISTMLPONNX.model";
-
-        //const std::wstring cntkModelFile = L"E:/LiqunWA/CNTK/ONNX/LogisticRegression.model";
-        //const std::wstring savedONNXModelFile = L"E:/LiqunWA/CNTK/ONNX/LogisticRegressionONNX.model";
-
-        FunctionPtr cntkModel = Function::Load(cntkModelFile, device, ModelFormat::CNTKv2);
-
-        cntkModel->Save(savedONNXModelFile, ModelFormat::ONNX);
-        // TODO PrintGraph does not work for some reason
-        // PrintGraph(cntkModel->RootFunction(), 0);
-
-        // failed here because graph save/load operation is not idempotent
-        FunctionPtr cntkModelFromONNX = Function::Load(savedONNXModelFile, device, ModelFormat::ONNX);
-        // PrintGraph(cntkModelFromONNX, 0);
-        cntkModelFromONNX->Save(cntkModelFile2, ModelFormat::CNTKv2);
-    }
-}
-
 int main(int argc, char *argv[])
 {
 #if defined(_MSC_VER)
@@ -108,8 +32,6 @@ int main(int argc, char *argv[])
         return -1;
     }
 #endif
-
-    RunLotus(DeviceDescriptor::CPUDevice());
 
     // Lets disable automatic unpacking of PackedValue object to detect any accidental unpacking
     // which will have a silent performance degradation otherwise
@@ -164,7 +86,7 @@ int main(int argc, char *argv[])
             fprintf(stderr, "Run test on a GPU device.\n");
             TrainCifarResnet();
         }
-        
+
         if (ShouldRunOnCpu())
         {
             fprintf(stderr, "Cannot run TrainCifarResnet test on a CPU device.\n");
