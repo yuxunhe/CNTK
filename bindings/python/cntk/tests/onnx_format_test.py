4270569c241f1a1065e427abc841fe3c15a63f9b
liqun fu <liqfu@microsoft.com>
2017-10-04 22:45:30 -0700

    add conv test

--- bindings/python/cntk/tests/onnx_format_test.py
+++ bindings/python/cntk/tests/onnx_format_test.py
@@ -37,3 +37,32 @@ def test_dense_layer(tmpdir):
     x_ = loaded_node.arguments[0];
     assert np.allclose(loaded_node.eval({x_:img}), root_node.eval({x:img}))
 
+def test_conv_layer(tmpdir):
+    image_width = 28
+    image_height = 28
+    input_dim_model = (1, image_width, image_height)
+    input_dim = image_width * image_height
+    num_output_classes = 10
+
+    features = C.input_variable(input_dim_model)
+
+    with C.layers.default_options(init=C.glorot_uniform(), activation=C.relu):
+        convModel = features
+        convModel = C.layers.Convolution2D(filter_shape=(5,5),
+                                   num_filters=8,
+                                   strides=(2,2),
+                                   pad=True, name='first_conv')(convModel)
+        convModel = C.layers.Convolution2D(filter_shape=(5,5),
+                                   num_filters=16,
+                                   strides=(2,2),
+                                   pad=True, name='second_conv')(convModel)
+        convModel = C.layers.Dense(num_output_classes, activation=None, name='classify')(convModel)
+
+    img_data = np.random.rand(image_width * image_height).reshape(1, 1, image_width, image_height).astype('float32')
+
+    filename = os.path.join(str(tmpdir), R'conv_layer.onnx')
+
+    convModel.save(filename, format=C.ModelFormat.ONNX)
+    loaded_node = C.Function.load(filename, format=C.ModelFormat.ONNX)
+
+    assert np.allclose(loaded_node.eval(img_data), convModel.eval(img_data))
