57ab7e41e8d97b969466fc887536326a219f288c
jaliyaek <jaliyaek@microsoft.com>
2017-10-06 14:44:02 -0700

    fixing review comments and adding test cases

--- bindings/python/cntk/netopt/factorization.py
+++ bindings/python/cntk/netopt/factorization.py
@@ -3,8 +3,10 @@ from cntk.ops.functions import BlockFunction
 from cntk.variables import Parameter
 from cntk.ops import times
 from cntk.internal import _as_tuple
-from cntk.layers.blocks import _initializer_for, _INFERRED, identity, UntestedBranchError  # helpers
-from cntk.default_options import is_default_override, get_default_override, default_override_or
+from cntk.layers.blocks import _initializer_for, _INFERRED, identity
+from cntk.layers.blocks import UntestedBranchError  # helpers
+from cntk.default_options import is_default_override
+from cntk.default_options import get_default_override, default_override_or
 
 def svd_subprojection(matrix, k):
     '''
@@ -12,64 +14,77 @@ def svd_subprojection(matrix, k):
 
     Args:
         matrix : an input matrix        
-        k (int): length of one dimension of the matrix after sub projection
+        k (int): desired rank of the output matrix
 
     Returns:
-        two matrices representing the original matrix after svd and subprojection.
+        two matrices representing the original matrix after svd and 
+        reducing them based on k.
     '''
-
+    
+    import numpy as np
     from numpy import dot, diag
     from numpy.linalg import svd
 
     # Decompose W into (U, s, V)
     U, s, V = svd(matrix, full_matrices=False)
-       
+          
     # Create two dense layers from this; one that takes U, one that takes
     # dot(s, V), but restrict them all to rank k, such that the result is a
     # k-rank subprojection
-    W1 = U[:, :k]
+    W1 = np.ascontiguousarray(U[:, :k])
     W2 = dot(diag(s[:k]), V[:k, :])
    
     return W1, W2
 
-def factor_dense(model, projection_function, filter_function = None, factor_function = None):
+
+def factor_dense(model, projection_function = None, filter_function = None, 
+                 factor_function = None):
     '''
     Factor a dense model to reduce its size based on the provided factor_function and the 
     projection_function. If no projection_funciton is specified, use svd decomposition. 
 
     Args:
-        model                   : dense model.
-        projection_function     : function to return the new size (K) of the dense model
-        filter_function         : function to filter layers in the model to apply the factorization
-        factor_function         : function to factor the dense model (e.g. svd)   
+        model               : dense model.
+        projection_function : determin the new size of the dense model. It can 
+                              be based on the shape of the weight matrix or 
+                              other heuristics.
+                              factor_function can choose to ignore the value k.
+        filter_function     : filter layers in the model to apply the factorization
+        factor_function     : factor the dense model (e.g. svd)   
                 
     Returns:
-        a model that is factored and projected (reduced).
+        a model that is factored and reduced in size.
     '''
-    dense_filter = (lambda x: type(x) == cntk.Function and x.op_name == 'Dense') 
+    if (factor_function == None and projection_function == None):
+        raise ValueError("Dense: default factor function (svd) requires a projection_function.")
     
+    dense_filter = (lambda x: type(x) == cntk.Function 
+                       and x.op_name == 'Dense' 
+                       and filter_function(x) if filter_function else True)
+   
     def dense_converter(model):        
         W, b = model.W.value, model.b.value
-        
-        # convert based on the filter condition.
-        if filter_function and not filter_function(W):
-            return model
 
         ht, wdth = W.shape
-        k = projection_function(W)       
-        W1, W2 = factor_function(W) if factor_function else svd_subprojection(W, k)
-       
+
+        # k is the rank of the output matrices. If a projection function is 
+        # provided, then use it, otherwise assign min of two dimensions of
+        # W to k.
+        k = projection_function(W) if projection_function else min(ht, wdth)
+        W1, W2 = factor_function(W, k) if factor_function else svd_subprojection(W, k)
+
         Ws = {'W1': W1, 'W2': W2}
-        dfl = _dense_factored((k, wdth),
+        dfl = dense_factored((k, wdth),
             init=Ws,
             activation=None,
             init_bias=b,
-            name='_factored_model')(model.inputs[2])
+            name='factored_model')(model.inputs[2])
         return dfl
 
     return cntk.misc.convert(model, dense_filter, dense_converter)
 
-def _dense_factored(shapes, #(shape1, shape2)
+
+def dense_factored(shapes, #(shape1, shape2)
                   activation=default_override_or(identity),
                   init={'W1':None, 'W2':None},
                   input_rank=None,
@@ -112,7 +127,8 @@ def _dense_factored(shapes, #(shape1, shape2)
         raise ValueError("Dense: input_rank and map_rank cannot be specified at the same time.")
 
 
-    # If input_rank not given then pass a single _INFERRED; map_rank if given will determine the input_rank.
+    # If input_rank not given then pass a single _INFERRED; 
+    # map_rank if given will determine the input_rank.
     # The dimension inference may still create multiple axes.
     input_shape = _INFERRED
 
@@ -121,10 +137,10 @@ def _dense_factored(shapes, #(shape1, shape2)
     init_weights = init
     W1 = Parameter(input_shape + output_shape1, init=init_weights['W1'], name='W1')
     W2 = Parameter(output_shape1 + output_shape2, init=init_weights['W2'], name='W2')
-    b = Parameter(              output_shape2, init=init_bias,    name='b') if bias else None
+    b = Parameter(output_shape2, init=init_bias,    name='b') if bias else None
 
     # expression of this function
-    @BlockFunction('_factored_model', name)
+    @BlockFunction('factored_model', name)
     def dense(x):
         r = times(x, W1)
         r = times(r, W2)
